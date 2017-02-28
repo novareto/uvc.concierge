28fc84ad4b51a69ab88aa76d5f6c2987a1a4c7b9
goschtl <ck@novareto.de>
2017-02-28 15:59:13 +0100

    chanegs

--- src/uvc/concierge/resources.py
+++ src/uvc/concierge/resources.py
@@ -4,8 +4,9 @@ from fanstatic import Library, Resource
 from js.jquery import jquery
 
 library = Library('remote_wsgi', 'assets')
-js = Resource(library, 'menu.js', depends=[jquery])
-css = Resource(library, 'menu.css')
-
-js = Resource(library, 'main.c5596499.js', bottom=True)
-css = Resource(library, 'main.65027555.css')
+library = Library('remote_wsgi', 'build/static')
+js = Resource(library, 'main.js', depends=[jquery])
+#css = Resource(library, 'menu.css')
+#
+#js = Resource(library, 'main.c5596499.js', bottom=True)
+#css = Resource(library, 'main.65027555.css')
